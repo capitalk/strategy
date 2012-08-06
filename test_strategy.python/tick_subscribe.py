d92cddf7e9df1956e2deb719ca5d79e9b2608c80
Timir Karia <timir@capitalkpartners.com>
2012-08-06 21:22:20 +0000

    Commented a bit more

--- test_strategy.python/tick_subscribe.py
+++ test_strategy.python/tick_subscribe.py
@@ -1,15 +1,23 @@
 import zmq
 import proto_objs.spot_fx_md_1_pb2
 
-
+# Create context and connect
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-#socket.connect("tcp://127.0.0.1:5273")
-socket.connect("tcp://83.160.107.178:9000")
+
+# Use below for direct market subscription
+
+socket.connect("tcp://127.0.0.1:5273")
+# Use below for aggregated book
+#socket.connect("tcp://83.160.107.178:9000")
+
 #socket.setsockopt(zmq.SUBSCRIBE, "")
-socket.setsockopt(zmq.SUBSCRIBE, "GBP/USD")
+# Use below for direct market subscription
+socket.setsockopt(zmq.SUBSCRIBE, "EUR/USD")
 
 while True:
+    # N.B. only aggregated book has topc and contents
+    # while direct market data sends only protobuf
     #[topic, contents] = socket.recv_multipart()
     contents = socket.recv()
     bbo = proto_objs.spot_fx_md_1_pb2.instrument_bbo();
